f69c562fc7f280b07c9b6c76f18430207c17dcbf
faustaleonardo <faustaleonardo11@gmail.com>
2026-06-30 07:38:29 +0700

    if, concat, includes

---
+++ questions/00898-easy-includes-DONE/template.ts
@@ -0,0 +1,10 @@
+import { Equal } from "@type-challenges/utils";
+
+export type Includes<T extends readonly any[], U> = T extends [
+  infer F,
+  ...infer R,
+]
+  ? Equal<F, U> extends true
+    ? true
+    : Includes<R, U>
+  : false;
