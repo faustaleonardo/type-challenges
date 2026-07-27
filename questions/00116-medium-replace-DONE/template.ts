68ed92d714274844f229afa8b48ae23d8c682d6f
faustaleonardo <faustaleonardo11@gmail.com>
2026-07-27 06:44:35 +0400

    capitalize, replace, replace-all

---
+++ questions/00116-medium-replace-DONE/template.ts
@@ -0,0 +1,9 @@
+type Replace<
+  S extends string,
+  From extends string,
+  To extends string,
+> = From extends ""
+  ? S
+  : S extends `${infer Prefix}${From}${infer Suffix}`
+    ? `${Prefix}${To}${Suffix}`
+    : S;
---
+++ questions/00119-medium-replaceall-DONE/template.ts
@@ -0,0 +1,9 @@
+type ReplaceAll<
+  S extends string,
+  From extends string,
+  To extends string,
+> = From extends ""
+  ? S
+  : S extends `${infer Prefix}${From}${infer Suffix}`
+    ? `${Prefix}${To}${ReplaceAll<Suffix, From, To>}`
+    : S;
